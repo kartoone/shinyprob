51202f00eb8f928b354fe60fa95f6911677821d7
kartoone <brtoone@samford.edu>
2018-11-09 06:34:13 -0600

    added stats analysis section.

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
   <style>
   label {display:inline-block;width:150px;font-size:1.1em;padding:6px}
   input {font-size:1.1em}
+  span {font-weight:bold;font-size:1.2em}
   </style>
 </head>
 <body>
   <h1>goxp.org - leaderboard tracking for pokemon go</h1>
   <p>
-  Shortly after having the idea to automate some of the leaderboard tracking tasks associated with Pokemon Go, I was hit by a car while riding my bike. That life-changing event and a lot of other things have pushed the project into the backburner. In the meantime, though enjoy a simulator below which will simulate people trying to find shiny (or perfect) pokemon.
-</p>
+  Lots of ideas for leaderboard tracking and automation, but so little time!!!
+  </p>
   <h1>Shiny Probability Simulator</h1>
   <p>
-    This website uses Javascript to runs repeated simulations to show how many
+    This website uses Javascript to run repeated simulations to show how many
     shinies a particular person finds given a known (or guessed) probability
     of each encounter being a shiny.
   </p>
@@ -22,9 +23,9 @@
     <label for="numsims">Number of People</label>
     <input type="number" id="numsims" name="numsims" value="25" /><br />
     <label for="threshold">Minimum shinies</label>
-    <input type="number" id="threshold" name="threshold" value="0" /> (Leave blank or 0 to force max encounters to run)<br />
+    <input type="number" id="threshold" name="threshold" value="0" /> (Leave blank or 0 to see how many shinies people find over given number of encounters below)<br />
     <label for="numsims">Max # encounters</label>
-    <input type="number" id="numruns" name="numruns" value="100" /><br />
+    <input type="number" id="numruns" name="numruns" value="150" /><br />
     <label for="numsims">Shiny probability</label>
     1 / <input type="number" id="simprob" name="simprob" value="50" />
     <input type="submit" value="Run" />
@@ -37,20 +38,29 @@
     <tbody>
     </tbody>
   </table>
-  <h2>Treshold Analysis</h2>
-  <p>This section shows analysis related to the threshold number of shinies you entered. If a given person (simulation) doesn't reach the threshold they are not included in this analysis. Put a really low threshold or a high max number of enounters if you want to make sure all simulations are counted.</p>
-  <table>
+  <h2>Simulation Analysis</h2>
+  <p>This section shows analysis related to the specified number of shinies you entered. If you left the "Minimum shinies" field blank or 0, then this section
+    summarizes how many shinies people found over the given number of max encounters. If you specified a non-zero number for "Minimum shinies", then each
+    individual simulation stops and reports how many encounters it took to find that number of shinies. This section displays statistics related to minimum and
+    maximum and average number of encounters to find the specified number of shinies.</p>
+  <table id="threshtbl">
     <tr>
-      <td>Threshold:</td>
-      <td id="anathresh"></td>
-    </tr>
-    <tr>
-      <td>Threshold count:</td>
+      <td># of people who found <span id="countspan"></span> shinies:</td>
       <td id="anacount"></td>
     </tr>
     <tr>
-      <td>Threshold stats:<br />Avg/Min/Max #encounters</td>
+      <td>Avg/Min/Max encounters before finding <span id="threshspan"></span> shinies:</td>
       <td id="anastats"></td>
+    </tr>
+  </table>
+  <table id="counttbl">
+    <tr>
+      <td>Average number of shinies found per person in <span id="simsspan"></span> encounters:</td>
+      <td id="avgshinycount"></td>
+    </tr>
+    <tr>
+      <td>Min/Max number of shinies found by single person in <span id="statsspan"></span> encounters:</td>
+      <td id="shinystats"></td>
     </tr>
   </table>
   <h2>Source Code</h2>
--- randomsim.js
+++ randomsim.js
@@ -1,4 +1,5 @@
 var workers = [];
+var workerstats = {};
 
 function runSimulation(event) {
   event.preventDefault();
@@ -6,14 +7,29 @@ function runSimulation(event) {
   var thresh = Number(document.getElementById("threshold").value);
   var runs = Number(document.getElementById("numruns").value);
   var prob = Number(document.getElementById("simprob").value);
+  document.getElementById('countspan').innerHTML = thresh;
+  document.getElementById('threshspan').innerHTML = thresh;
+  document.getElementById('simsspan').innerHTML = runs;
+  document.getElementById('statsspan').innerHTML = runs;
+  if (thresh) {
+    document.getElementById('counttbl').style.display = 'none';
+    document.getElementById('threshtbl').style.display = '';
+  } else {
+    document.getElementById('counttbl').style.display = '';
+    document.getElementById('threshtbl').style.display = 'none';
+  }
   killWorkers();
   resetRows();
+  workerstats.threshold = thresh;
+  workerstats.successful = []; // init an empty array of workers who have reached the threshold
   for (var i = 0; i < sims; i++) {
     workers[i] = new Worker("randworker.js");
+    workers[i].stats = {}; // setup a new place to keep track of this workers stats
     addRow(i);
     workers[i].addEventListener('message',function(e){
       // worker sends back [id, successes, total, currentrand, currentprob]
-      updateRow(e.data[0],e.data);
+      updateRow(e.data);
+      updateStats(e.data);
     });
     workers[i].postMessage([i,runs,prob,thresh]);
   }
@@ -35,7 +51,8 @@ function addRow(i) {
   cell.setAttribute('id','cur'+i);
 }
 
-function updateRow(i,data) {
+function updateRow(data) {
+  var i = data[0]; // worker who sent their data to us
   var totalcell = document.getElementById('total'+i);
   totalcell.innerHTML = data[2];
   var succcell = document.getElementById('succ'+i);
@@ -52,6 +69,62 @@ function updateRow(i,data) {
   probcell.innerHTML = data[4];
 }
 
+function updateStats(data) {
+  updateAllStats(data);
+  updateSuccessfulStats(data);
+}
+
+function updateAllStats(data) {
+  workers[data[0]].stats.total = data[2];
+  workers[data[0]].stats.succ = data[1];
+  let maxshinies = false;
+  let minshinies = false;
+  let totshinies = 0;
+  for (let w of workers) {
+    if (maxshinies===false || w.stats.succ>maxshinies) {
+      maxshinies = w.stats.succ;
+    }
+    if (minshinies===false || w.stats.succ<minshinies) {
+      minshinies = w.stats.succ;
+    }
+    totshinies += w.stats.succ;
+  }
+  let avg = Math.round(totshinies*10/workers.length)/10.0;
+  document.getElementById('avgshinycount').innerHTML = avg;
+  document.getElementById('shinystats').innerHTML = minshinies + " / " + maxshinies;
+}
+
+// keep track of how many workers have reached threshold
+function updateSuccessfulStats(data) {
+  let hit = false;
+  for (let w of workerstats.successful) {
+    if (w.id == data[0]) {
+      w.total = data[2];
+      w.succ = data[1];
+      hit = true;
+      break;
+    }
+  }
+  if (!hit && data[1] >= workerstats.threshold) {
+    workerstats.successful.push({id:data[0],total:data[2],succ:data[1]});
+  }
+  document.getElementById('anacount').innerHTML = workerstats.successful.length;
+  let totalruns = 0;
+  let maxruns = false;
+  let minruns = false;
+  for (let w of workerstats.successful) {
+    if (maxruns===false || w.total>maxruns) {
+      maxruns = w.total;
+    }
+    if (minruns===false || w.total<minruns) {
+      minruns = w.total;
+    }
+    totalruns += w.total;
+  }
+  let avgruns = workerstats.successful.length>0?Math.round(totalruns/workerstats.successful.length):0;
+  document.getElementById('anastats').innerHTML = avgruns + " / " + minruns + " / " + maxruns;
+}
+
 function resetRows() {
   var tbl = document.getElementById('simtbl');
   while (tbl.rows.length>1) {
@@ -63,4 +136,6 @@ function killWorkers() {
   for (var i=0;i<workers.length;i++) {
     workers[i].terminate();
   }
+  workers = [];
+  workerstats = {};
 }
